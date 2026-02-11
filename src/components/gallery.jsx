72a49ca259615666e2c2f9781f63a30d1404e6ad
StepNepal <stepnepal2381@gmail.com>
2026-02-11 14:13:04 +0545

    Update gallery.jsx

--- src/components/gallery.jsx
+++ src/components/gallery.jsx
@@ -1,17 +1,28 @@
 import { Image } from "./image";
-import React from "react";
+import React, { useState } from "react";
 
-export const Gallery = (props) => {
+export const Gallery = ({ data }) => {
+  const [visibleCount, setVisibleCount] = useState(6); // how many images to show
+
+  const handleShowMore = () => {
+    if (visibleCount >= data.length) {
+      setVisibleCount(6); // reset
+    } else {
+      setVisibleCount(data.length); // show all
+    }
+  };
+
   return (
     <div id="portfolio" className="text-center">
       <div className="container">
         <div className="section-title">
           <h2>Gallery</h2>
         </div>
+
         <div className="row">
           <div className="portfolio-items">
-            {props.data
-              ? props.data.map((d, i) => (
+            {data
+              ? data.slice(0, visibleCount).map((d, i) => (
                   <div
                     key={`${d.title}-${i}`}
                     className="col-sm-6 col-md-4 col-lg-4"
@@ -26,6 +37,16 @@ export const Gallery = (props) => {
               : "Loading..."}
           </div>
         </div>
+
+        {data && data.length > 6 && (
+          <button
+            onClick={handleShowMore}
+            className="btn btn-custom btn-lg"
+            style={{ marginTop: "20px" }}
+          >
+            {visibleCount >= data.length ? "Show Less" : "Show More"}
+          </button>
+        )}
       </div>
     </div>
   );
